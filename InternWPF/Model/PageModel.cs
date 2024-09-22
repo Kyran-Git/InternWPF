c1259621f3e6c8f6367af5fb0eed7971e49cb9bc
Kyran-Git <niklauscareer@gmail.com>
2024-09-22 21:09:15 +0800

    added all necessary classes and methods

--- InternWPF/Model/PageModel.cs
+++ InternWPF/Model/PageModel.cs
@@ -1,13 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace InternWPF.Model
 {
+    // User model to store user details
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+    }
+
+    // JournalEntry model to store journal entry details
+    public class JournalEntry
+    {
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Activity { get; set; }
+    }
+
+    // PageModel to hold the data related to users and their journal entries
     public class PageModel
     {
-        public int userCount {  get; set; }
+        // Tracks the number of users
+        public int UserCount { get; set; }
+
+        // List of users in the system
+        public List<User> Users { get; set; }
+
+        // List of journal entries for each user (could be tied to a specific user later if needed)
+        public List<JournalEntry> JournalEntries { get; set; }
+
+        // Constructor to initialize the lists
+        public PageModel()
+        {
+            Users = new List<User>();
+            JournalEntries = new List<JournalEntry>();
+        }
+
+        // Method to add a new user
+        public void AddUser(User user)
+        {
+            Users.Add(user);
+            UserCount = Users.Count;
+        }
+
+        // Method to add a journal entry
+        public void AddJournalEntry(JournalEntry entry)
+        {
+            JournalEntries.Add(entry);
+        }
     }
 }
