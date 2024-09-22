ff827fff77ffb26f6b54a733757447ee17d1516c
Kyran-Git <niklauscareer@gmail.com>
2024-09-22 19:42:01 +0800

    Added a bunch of folders and files to jumpstart the MVVM framework

---
+++ InternWPF/Model/PageModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternWPF.Model
+{
+    public class PageModel
+    {
+        public int userCount {  get; set; }
+    }
+}
