8d64cee822fd27a401d49491287a627b4575c761
Kyran-Git <niklauscareer@gmail.com>
2024-09-23 09:14:43 +0800

    transparent main window

--- InternWPF/Model/PageModel.cs
+++ InternWPF/Model/PageModel.cs
@@ -22,30 +22,24 @@ namespace InternWPF.Model
     // PageModel to hold the data related to users and their journal entries
     public class PageModel
     {
-        // Tracks the number of users
         public int UserCount { get; set; }
 
-        // List of users in the system
         public List<User> Users { get; set; }
 
-        // List of journal entries for each user (could be tied to a specific user later if needed)
         public List<JournalEntry> JournalEntries { get; set; }
 
-        // Constructor to initialize the lists
         public PageModel()
         {
             Users = new List<User>();
             JournalEntries = new List<JournalEntry>();
         }
 
-        // Method to add a new user
         public void AddUser(User user)
         {
             Users.Add(user);
             UserCount = Users.Count;
         }
-
-        // Method to add a journal entry
+        
         public void AddJournalEntry(JournalEntry entry)
         {
             JournalEntries.Add(entry);
