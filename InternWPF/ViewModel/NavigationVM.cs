2abc5302070e3357cd527da719485cac8d39cc25
Kyran-Git <niklauscareer@gmail.com>
2024-09-24 15:46:41 +0800

    fixed some things

--- InternWPF/ViewModel/NavigationVM.cs
+++ InternWPF/ViewModel/NavigationVM.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Page_Navigation_App.Utilities;
+using System.Windows.Input;
 
 namespace InternWPF.ViewModel
 {
-    internal class NavigationVM
+    class NavigationVM : VMBase
     {
+
     }
 }
