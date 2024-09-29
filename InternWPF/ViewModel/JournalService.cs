c99e7a7174dfe83029847c25114f6f8839b6a3d9
Kyran-Git <niklauscareer@gmail.com>
2024-09-30 01:39:00 +0800

    Latest stable

---
+++ InternWPF/ViewModel/JournalService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.ObjectModel;
+
+namespace InternWPF.ViewModel
+{
+    public class JournalService
+    {
+        public ObservableCollection<Journal> Journals { get; set; } = new ObservableCollection<Journal>();
+        public Journal SelectedJournal { get; set; }
+    }
+}
