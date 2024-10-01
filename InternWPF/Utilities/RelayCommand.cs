6214be38218d5df98fa533dbb719dc38248a55c2
Kyran-Git <niklauscareer@gmail.com>
2024-10-01 23:34:48 +0800

    latest stable

--- InternWPF/Utilities/RelayCommand.cs
+++ InternWPF/Utilities/RelayCommand.cs
@@ -5,25 +5,29 @@ namespace InternWPF.ViewModel
 {
     public class RelayCommand : ICommand
     {
-        private Action<object> execute;
-        private Func<object, bool> canExecute;
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
 
         public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
         {
-            this.execute = execute;
-            this.canExecute = canExecute;
+            _execute = execute;
+            _canExecute = canExecute;
         }
-
-        public event EventHandler CanExecuteChanged;
 
         public bool CanExecute(object parameter)
         {
-            return canExecute == null || canExecute(parameter);
+            return _canExecute == null || _canExecute(parameter);
         }
 
         public void Execute(object parameter)
         {
-            execute(parameter);
+            _execute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
         }
     }
 }
--- InternWPF/ViewModel/NavigationVM.cs
+++ InternWPF/ViewModel/NavigationVM.cs
@@ -1,13 +1,50 @@
-﻿using InternWPF.Model;
-using InternWPF.View;
-using InternWPF.Utilities;
-using System.Collections.ObjectModel;
+﻿using System;
+using System.ComponentModel;
 using System.Windows.Input;
 
 namespace InternWPF.ViewModel
 {
-    class NavigationVM : Utilities.VMBase
+    public class NavigationVM : INotifyPropertyChanged
     {
-        
+        private object _currentView;
+
+        // Property to store the current view model
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged(nameof(CurrentView));
+            }
+        }
+
+        // Commands to switch between views
+        public ICommand HomeCommand { get; }
+        public ICommand JournalsCommand { get; }
+        public ICommand SettingsCommand { get; }
+
+        // Constructor to initialize commands and set default view
+        public NavigationVM()
+        {
+            // Default view
+            CurrentView = new HomeVM();
+
+            HomeCommand = new RelayCommand(o => SwitchToHomeView());
+            JournalsCommand = new RelayCommand(o => SwitchToJournalsView());
+            SettingsCommand = new RelayCommand(o => SwitchToSettingsView());
+        }
+
+        // Methods to switch between views
+        private void SwitchToHomeView() => CurrentView = new HomeVM();
+        private void SwitchToJournalsView() => CurrentView = new JournalVM();
+        private void SwitchToSettingsView() => CurrentView = new SettingVM();
+
+        // INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
